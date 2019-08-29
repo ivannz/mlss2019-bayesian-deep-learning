088a57344336d656c2c42d953c9a2bd6012d399a
Ivan <ivannz@yandex.ru>
2019-08-29 09:48:19 +0300

    assert nonempty dataset

--- mlss2019bdl/dataset.py
+++ mlss2019bdl/dataset.py
@@ -67,6 +67,8 @@ def get_dataset(n_train=20, n_valid=5000, random_state=None, name="MNIST", path=
 
 def remove(indices, dataset):
     """Extract the specified samples from the dataset and remove."""
+    assert len(dataset) > 0
+
     mask = torch.zeros(len(dataset), dtype=torch.uint8)
     mask[indices] = True
 
